0b62601040b608d7d90f679ea076525abe6a9f1c
Rabin <rabin.dongol@btech.com.np>
2022-01-12 15:26:21 +0545

    initial commit

---
+++ src/test/java/com/rdongol/crudDemo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdongol.crudDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
